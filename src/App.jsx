d9016032b4f93146c0685f96e83333a73c960d2e
Vinícius dos Santos Oliveira <3izuna@gmail.com>
2026-01-31 15:29:30 -0300

    Delete the sections...

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
 import Header from "./assets/components/header/Header"
-import HumanKaneki from "./assets/components/main/HumanKaneki"
 import '../src/styles/main/main.css'
 import './styles/global.css'
-import GhoulKaneki from "./assets/components/main/GhoulKaneki"
+
 function App() {
   
   return (
     <>
-    <Header />
-    <main>
-      <HumanKaneki /> 
-      <GhoulKaneki />
-    </main>
+     <Header />
+     <main>
+  
+     </main>
     </>
   )
 }
