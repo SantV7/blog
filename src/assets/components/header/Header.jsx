26b7a920231adc1ac624f3176fd91353a9e6adf1
Vinícius dos Santos Oliveira <3izuna@gmail.com>
2026-02-01 12:38:38 -0300

    Adding Gsap settings

--- src/assets/components/header/Header.jsx
+++ src/assets/components/header/Header.jsx
@@ -1,14 +1,28 @@
+import { useEffect } from 'react';
 import '../../../styles/header/header.css'
+import { gsap } from 'gsap';
 
 const Header = () => {
+
+useEffect(() => {
+  gsap.to("#ghoul-red", {
+    delay: 0.6,
+    duration: 2.11,
+    opacity: 1,
+    ease: 'power2.out'
+  })
+},[])
+
+
+
   return (
     <>
     <header>
         <h1>Ken Kaneki</h1>
-        <span id='human-ghoul-span'>Human   
-          <span className='font-ease-lake'> | </span>
+        <div id='human-ghoul'>Human   
+          <div className='font-ease-lake'> | </div>
           <div className='font-ease-lake' id="ghoul-red"> Ghoul</div>
-        </span>
+        </div>
     </header>
     </>
   )
