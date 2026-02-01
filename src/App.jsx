42f7cbfa5cc203d1ef8904be0ea229dc48895ae5
Vinícius dos Santos Oliveira <3izuna@gmail.com>
2026-02-01 02:06:38 -0300

    ,,,...

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,13 @@ import Header from "./assets/components/header/Header"
 import '../src/styles/main/main.css'
 import './styles/global.css'
 import HumanKaneki from "./assets/components/main/HumanKaneki"
+import { gsap } from "gsap";
+    
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+import { ScrollSmoother } from "gsap/ScrollSmoother";
+
+gsap.registerPlugin(ScrollTrigger,ScrollSmoother);
 
 function App() {
   
--- src/assets/components/main/HumanKaneki.jsx
+++ src/assets/components/main/HumanKaneki.jsx
@@ -1,8 +1,14 @@
-
+import '../../../styles/main/humanKaneki.css'
 const HumanKaneki = () => {
+
+
   return (
      <>
        <section id='section-human'>
+        <header>
+          <div></div>
+          <div className="ageHuman">18y</div>
+        </header>
           <div className='text-card'>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et perspiciatis repellendus sed quibusdam quisquam pariatur, illo soluta voluptatem quae deleniti, iusto vitae reiciendis molestiae ad repudiandae aperiam at odio amet.</p>
           </div>
